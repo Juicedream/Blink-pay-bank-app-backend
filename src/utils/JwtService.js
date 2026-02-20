3c37db678847f851fa1549dc0cf14cb821f3a903
Juicedream <114106555+Juicedream@users.noreply.github.com>
2026-02-20 11:30:54 +0100

    Fix: Token _id not found fixed in JwtService

--- src/utils/JwtService.js
+++ src/utils/JwtService.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET= "myownsecretchaiinawaooo"
 class JWTService {
-    static generateToken(user){
-     const token =  jwt.sign({user}, JWT_SECRET, {
+    static generateToken(_id){
+     const token =  jwt.sign({_id}, JWT_SECRET, {
             algorithm: "HS256",
             expiresIn: "1d"
         });
